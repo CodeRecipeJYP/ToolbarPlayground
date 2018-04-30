cfc6b270df71ad54b632bfb1d293d906010e7b48
kujyp <pjo901018@korea.ac.kr>
2018-04-30 15:27:12 +0900

    Add Action button

---
+++ app/src/main/java/com/asuscomm/yangyinetwork/toolbarplayground/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.asuscomm.yangyinetwork.toolbarplayground
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                message.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                message.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                message.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val ab = supportActionBar
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actionbar_menu, menu)
+        val item = menu?.findItem(R.id.action_checkbox_all)
+        Log.d("", "" + item?.actionView)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
